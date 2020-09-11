23d24f4b0541e5fef19188d6b94d79f1ebf48ffa
dymoc <dymoc@mail.ru>
2020-09-11 07:14:19 +0300

    наполнение каталога, после иметируемого запроса на сервер

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,31 +1,13 @@
 "use strict";
 
-const items = [{
-        id: 1,
-        title: 'Notebook',
-        price: 20000,
-        img: '../src/assets/imgs/notebook.jpg',
-        quantity: 1,
-    },
-    {
-        id: 2,
-        title: 'Mouse',
-        price: 1500,
-        quantity: 1,
-    },
-    {
-        id: 3,
-        title: 'Keyboard',
-        price: 5000,
-        quantity: 1,
-    },
-    {
-        id: 4,
-        title: 'Gamepad',
-        price: 4500,
-        quantity: 1,
-    },
-];
+function renderProductMy(title, price, img = '../src/assets/imgs/error.png') { //если изображение отстутствует
+    return `<div class="product-item">
+                <img class="product-img" src = ${img}>
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="by-btn">Добавить в корзину</button>
+              </div>`;
+}
 
 
 class Cart {
@@ -34,13 +16,16 @@ class Cart {
     }
 
     _fillCart(items) {
-        content
-            .then(                
-                items.forEach(item => {
-                    this.items.push(new ItemCart(item.id, item.title, item.price, item.quantity));
-                })
-            )
+        items.forEach(item => {
+            this.items.push(new ItemCart(item.id, item.title, item.price, item.quantity));
+        });
     };
+
+    _render(items) {
+        items.forEach(item => {
+            document.querySelector('.som__products').innerHTML += renderProductMy(item.title, item.price, item.img);
+        });
+    }
 
     _addItem(item) {
         this.items.push(item);
@@ -76,11 +61,11 @@ class ItemCart {
         this.quantity = quantity;
     }
 
-    _render() {
+    render(item) {
         return `<div class="product-item">
-              <img class="product-img" src = ${img}>
-              <h3>${title}</h3>
-              <p>${price}</p>
+              <img class="product-img" src = ${item.img}>
+              <h3>${item.title}</h3>
+              <p>${item.price}</p>
               <button class="by-btn">Добавить в корзину</button>
             </div>`;
     }
@@ -88,21 +73,52 @@ class ItemCart {
 
 const myCart = new Cart();
 
-let content = (items) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (items) {
-                resolve();
-            } else {
-                reject();
-            }
-        }, 1000);
+let fillCart = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const items = [{
+                id: 1,
+                title: 'Notebook',
+                price: 20000,
+                img: '../src/assets/imgs/notebook.jpg',
+                quantity: 1,
+            },
+            {
+                id: 2,
+                title: 'Mouse',
+                price: 1500,
+                quantity: 1,
+            },
+            {
+                id: 3,
+                title: 'Keyboard',
+                price: 5000,
+                quantity: 1,
+            },
+            {
+                id: 4,
+                title: 'Gamepad',
+                price: 4500,
+                quantity: 1,
+            },
+        ];
+        console.log(items);
+        resolve(items);
+    }, 4000);
 
-    });
-}
+});
 
-myCart._fillCart(content(items));
-myCart.showCart();
+fillCart
+    .then(
+        items => {
+            myCart._fillCart(items);
+            myCart._render(items);
+            myCart.showCart();
+        }
+    )
+   
+
+
+
 
 
 // myCart._addItem(new ItemCart(5, 'desk', 1500, 3));
